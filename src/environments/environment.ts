a4c44570be06f84fbfde97348f75fa648104e7e3
Jolte Teofil <jolteteofil@yahoo.com>
2023-05-04 17:05:55 +0300

    Env variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { MinaEnv } from '@shared/types/core/environment/mina-env.type';
 export const environment: Readonly<MinaEnv> = {
   production: false,
   identifier: 'Open Mina Fuzzing',
-  // server: 'http://localhost:1700',
-  server: origin + '/assets/reports',
+  server: 'http://localhost:1700', // this is for testing with the NodeJS server that check the files anywhere in the computer
+  // server: origin + '/assets/reports', //this is for testing with files from frontend's assets folder
   parentDirectoryAbsolutePath: 'D:\\mina',
 };
