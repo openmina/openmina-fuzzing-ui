4c2bb3e34a5b11cfd0813a5b9bbb0d1bf8746e60
Jolte Teofil <jolteteofil@yahoo.com>
2023-05-04 17:23:07 +0300

    Remove ending slash

--- src/app/shared/constants/config.ts
+++ src/app/shared/constants/config.ts
@@ -3,6 +3,7 @@ import { environment } from '@environment/environment';
 
 export const CONFIG: Readonly<MinaEnv> = {
   ...environment,
+  parentDirectoryAbsolutePath: environment.parentDirectoryAbsolutePath?.replace(/\/$/, ''),
 };
 
 (window as any).config = CONFIG;
