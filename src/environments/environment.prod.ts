75fd321d3190acf7711526022e39a783300fe71c
Jolte Teofil <jolteteofil@yahoo.com>
2023-05-04 12:08:48 +0300

    Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment: Readonly<MinaEnv> = {
   production: true,
   identifier: window['env']['identifier'] || 'Open Mina Fuzzing',
   server: window['env']['server'] || (origin + '/assets/reports'),
-  parentDirectoryAbsolutePath: window['env']['filesAbsolutePath'] || './',
+  parentDirectoryAbsolutePath: window['env']['parentDirectoryAbsolutePath'] || './',
 };
